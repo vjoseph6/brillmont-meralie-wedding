0d0a54e83bd733da52e68bc9ea715f4578c0467f
vjoseph6 <vjosephlhee@gmail.com>
2025-10-13 01:02:19 +0800

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div className="end-text">
+      <p>
+        Made with ❤️ by Joseph Lhee Villariasa | © {new Date().getFullYear()}
+        Brillmont & Meralie Wedding
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/configs/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCL_afGoyfLkBsKBfxbHRroM6K8XyNw10E",
+  authDomain: "bm-wedding.firebaseapp.com",
+  projectId: "bm-wedding",
+  storageBucket: "bm-wedding.firebasestorage.app",
+  messagingSenderId: "75720334313",
+  appId: "1:75720334313:web:97d2051e150c9605bcc978",
+  measurementId: "G-6KZ1SJGLSW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
